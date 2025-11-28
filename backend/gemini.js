438aa9fe4ee3b2b13bf91a410b653a18657c1fa7
nimisha <agnihotrinimisha296@gmail.com>
2025-11-28 16:02:26 +0530

    connected gemini api

--- backend/gemini.js
+++ backend/gemini.js
@@ -1,1 +1,1 @@
-const geminiUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
+const geminiUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyBPAsWt7CpfLZkZFQSCeAPLmnEOaHA4XQ8";
